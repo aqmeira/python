9217ac422e1f4d30a1306818644a664708a6ef73
aqmeira <81516274+aqmeira@users.noreply.github.com>
2021-11-05 10:48:49 -0300

    Adicionado arquivo sped icms

---
+++ sped.py
@@ -0,0 +1,30 @@
+arquivo = open('santaclara102021.txt','r+')
+
+novoArquivo = []
+
+for registro in arquivo:
+
+    if registro[:6] == '|C190|': ##or '|C100|' :
+        newFile = list(registro.split('|'))
+
+        for dados in newFile:
+            if dados == '1102' or dados == '6102' : ##or dados == '55':
+                 newFile[2]  = '090'
+                 newFile[6]  = '0,00'
+                 newFile[7]  = '0,00'
+                ##newFile[21] = '0,00'
+                #newFile[22] = '0,00'
+        x = ('|'.join(newFile))
+        novoArquivo.append(x)
+        continue
+    novoArquivo.append(registro)
+
+arquivo.close
+
+novoSped = open ('Narquivo.txt', 'w')
+for linha in novoArquivo:
+    novoSped.writelines(linha)
+
+novoSped.close()
+
+print('concluido')
